ad3b22f36cd375dcddbdbed5658fb7076b5673a2
Mihailer <node.q13@gmail.com>
2023-02-04 19:59:11 +0300

    refresh: client controller

--- controllers/client-controller.js
+++ controllers/client-controller.js
@@ -3,38 +3,40 @@ const ClientModel = require('../models/client-model.js')
 class ClientController {
     async create(req, res) {
         try {
-            console.log( req.body );
+            console.log( req.user.id );
             const {
+                user_id = req.user.id,
                 client_number,
                 client_name,
                 client_tel_number,
-                client_reason,
-                client_device_type,
-                client_device_manufacturer,
-                client_device_model,
-                client_approximate_cost_service,
-                client_approximate_return_date,
-                client_prepayment_amount,
-                client_prepayment_method,
-                client_interview,
+                // client_reason,
+                // client_device_type,
+                // client_device_manufacturer,
+                // client_device_model,
+                // client_approximate_cost_service,
+                // client_approximate_return_date,
+                // client_prepayment_amount,
+                // client_prepayment_method,
+                // client_interview,
           
             } = req.body
 
-            console.log( client_tel_number )
+            // console.log( client_tel_number )
 
             const newClient = await ClientModel.create({
+                user_id,
                 client_number,
                 client_name,
                 client_tel_number,
-                client_reason,
-                client_device_type,
-                client_device_manufacturer,
-                client_device_model,
-                client_approximate_cost_service,
-                client_approximate_return_date,
-                client_prepayment_amount,
-                client_prepayment_method,
-                client_interview,
+                // client_reason,
+                // client_device_type,
+                // client_device_manufacturer,
+                // client_device_model,
+                // client_approximate_cost_service,
+                // client_approximate_return_date,
+                // client_prepayment_amount,
+                // client_prepayment_method,
+                // client_interview,
     
             });
             
@@ -47,7 +49,8 @@ class ClientController {
 
     async getAllClients(req, res) {
         try {
-            const client = await ClientModel.find({}, ' -_id -__v') ;
+            console.log( req.user.id );
+            const client = await ClientModel.find( { user_id: { $eq: req.user.id}} ) ;
             return res.json(client);
         } catch (e) {
             res.status(500).json(e);
@@ -61,31 +64,31 @@ class ClientController {
                 client_number,
                 client_name,
                 client_tel_number,
-                client_reason,
-                client_device_type,
-                client_device_manufacturer,
-                client_device_model,
-                client_approximate_cost_service,
-                client_approximate_return_date,
-                client_prepayment_amount,
-                client_prepayment_method,
-                client_interview,
-                client_comment 
+                // client_reason,
+                // client_device_type,
+                // client_device_manufacturer,
+                // client_device_model,
+                // client_approximate_cost_service,
+                // client_approximate_return_date,
+                // client_prepayment_amount,
+                // client_prepayment_method,
+                // client_interview,
+                // client_comment 
             } = req.body
             const newClient = {
                 client_number,
                 client_name,
                 client_tel_number,
-                client_reason,
-                client_device_type,
-                client_device_manufacturer,
-                client_device_model,
-                client_approximate_cost_service,
-                client_approximate_return_date,
-                client_prepayment_amount,
-                client_prepayment_method,
-                client_interview,
-                client_comment 
+                // client_reason,
+                // client_device_type,
+                // client_device_manufacturer,
+                // client_device_model,
+                // client_approximate_cost_service,
+                // client_approximate_return_date,
+                // client_prepayment_amount,
+                // client_prepayment_method,
+                // client_interview,
+                // client_comment 
             }
 
             await ClientModel.findByIdAndUpdate(
@@ -103,16 +106,16 @@ class ClientController {
                         client_number,
                         client_name,
                         client_tel_number,
-                        client_reason,
-                        client_device_type,
-                        client_device_manufacturer,
-                        client_device_model,
-                        client_approximate_cost_service,
-                        client_approximate_return_date,
-                        client_prepayment_amount,
-                        client_prepayment_method,
-                        client_interview,
-                        client_comment 
+                        // client_reason,
+                        // client_device_type,
+                        // client_device_manufacturer,
+                        // client_device_model,
+                        // client_approximate_cost_service,
+                        // client_approximate_return_date,
+                        // client_prepayment_amount,
+                        // client_prepayment_method,
+                        // client_interview,
+                        // client_comment 
                     })
                 }
             })
